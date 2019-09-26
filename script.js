afe2d8f06b57c64643dac168fb6638eb62e03481
Alec McDaugale <amcdaugale@student.cccs.edu>
2019-09-25 18:27:41 -0600

    adding updated code. this is auto generated message.

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function setHTML(id, text)
   @param external_file_item The file you wish to coppy.
 */
 function getHTML(local_item, external_file_item)
-{ doccument.write($().load(external_file_item))
+{ $('body').load(external_file_item)
   //$.noConflict();
   //$(document).ready(function(){
    // $(local_item).load(external_file_item);
